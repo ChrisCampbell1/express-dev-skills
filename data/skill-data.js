507f1d69b08759f6c285b57157d15a36252a550c
Chris Campbell <christopherecampbell@gmail.com>
2023-01-05 17:08:17 -0700

    add skill data

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {name: "HTML", type: "Front End", proficiency: "High"},
+  {name: "CSS", type: "Front End", proficiency: "Medium"},
+  {name: "Express.js", type: "Back End", proficiency: "Medium"}
+]
+
+export {
+  skills
+}
--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
+import { skills } from '../data/skill-data.js'
 
 const router = Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('/', {
+  res.render('/index', {
     skills: skills
   })
 })
